c9bed7d246e07631bf3142bb43942728b951d5c5
Ayra Baet <ayra.austine@gmail.com>
2025-12-30 17:21:03 +0800

    Initial commit

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return(
+    <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-violet-600 to-indigo-600">
+      <h1 className="text-2xl text-white">Currency Converter</h1>
+    </div>
+  );
+}
+
+export default App;
