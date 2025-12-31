49d812089678004dc21408b69b53fc425eb01e8f
Ayra Baet <ayra.austine@gmail.com>
2025-12-31 14:18:48 +0800

    Added state variables

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [baseCurrency, setBaseCurrency] = useState('USD');
+  const [targetCurrency, setTargetCurrency] = useState('PHP');
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  
   return(
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-violet-600 to-indigo-600">
       <h1 className="text-2xl text-white">Currency Converter</h1>
