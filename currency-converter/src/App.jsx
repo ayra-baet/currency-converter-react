cdb9ae510224ac1aa720b95e11b42d8ce4aa59e5
Ayra Baet <ayra.austine@gmail.com>
2026-01-01 15:03:20 +0800

    Imported and used the useCurrencyInfo

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import { useCurrencyInfo } from "./hooks/useCurrencyInfo";
 
 function App() {
   const [amount, setAmount] = useState(0);
   const [baseCurrency, setBaseCurrency] = useState('USD');
   const [targetCurrency, setTargetCurrency] = useState('PHP');
   const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(baseCurrency);
+  const currencyCodes = currencyInfo ? Object.keys(currencyInfo) : [];
   
   return(
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-violet-600 to-indigo-600">
